cdcb257b80b9addf11fee0fdbd1d27637c82c2aa
derry32 <derry32@gmail.com>
2024-08-15 12:45:32 +0700

    webtables commit

--- src/main/java/com/juaracoding/WebTables.java
+++ src/main/java/com/juaracoding/WebTables.java
@@ -16,10 +16,14 @@ public class WebTables {
         driver.get("https://demoqa.com/webtables");
         System.out.println("Open Browser URL");
 
-        js.executeScript("window.scrollBy(0,500)");
+        js.executeScript("window.scrollBy(0,350)");
         Thread.sleep(2000);
         driver.findElement(By.id("addNewRecordButton")).click();
-        System.out.println("Berhasil membuka Modal PopUp");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("body > div.fade.modal.show > div > div > div.modal-header > button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("addNewRecordButton")).click();
+        System.out.println("Berhasil membuka dan menutup Modal PopUp");
 
 
         driver.findElement(By.id("firstName")).sendKeys("Derry");
@@ -36,6 +40,34 @@ public class WebTables {
         Thread.sleep(2000);
         driver.findElement(By.id("submit")).click();
         System.out.println("Berhasil Submit Form");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("edit-record-4")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("body > div.fade.modal.show > div > div > div.modal-header > button")).click();
+        System.out.println("Berhasil membuka dan menutup modal popup edit");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("edit-record-4")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("age")).clear();
+        Thread.sleep(1000);
+        driver.findElement(By.id("age")).sendKeys("31");
+        Thread.sleep(1000);
+        driver.findElement(By.id("salary")).clear();
+        Thread.sleep(1000);
+        driver.findElement(By.id("salary")).sendKeys("2000000");
+        Thread.sleep(1000);
+        driver.findElement(By.id("department")).clear();
+        Thread.sleep(1000);
+        driver.findElement(By.id("department")).sendKeys("SQA");
+        Thread.sleep(2000);
+        driver.findElement(By.id("submit")).click();
+        System.out.println("Berhasil Edit Form");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("delete-record-4")).click();
+        System.out.println("Berhasil menghapus");
 
         try {
             Thread.sleep(5000);
